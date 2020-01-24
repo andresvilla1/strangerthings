5c5d8afc474698f431d696b5b6e65c90ee1d9c31
andresvilla1 <37849309+andresvilla1@users.noreply.github.com>
2020-01-24 15:09:47 -0800

    Update stranger_things.rb

--- stranger_things.rb
+++ stranger_things.rb
@@ -1,37 +1,97 @@
-use_bpm 84
-use_synth :tech_saws
-
+sample "C:/Users/andres_villa/Desktop/dark_side.wav"
+sleep 4
 live_loop :bass do
-  play :E2, sustain: 2
-  play :C3, sustain: 2
-  
-  sleep 6
-  play :B3, sustain: 2
-  play :D3, sustain: 2
-  
-  sleep 2
-  play :E3, sustain: 2
-  play :B3, sustain: 2
-  
-  sleep 8
+  play :E2
+  sleep 0.5
 end
-live_loop :mainbeat do
-  use_synth :tech_saws
-  play :C4
-  sleep 0.25
-  play :E4
-  sleep 0.25
-  play :G4
-  sleep 0.25
-  play :B4
-  sleep 0.25
-  play :C5
-  sleep 0.25
-  play :B4
-  sleep 0.25
-  play :G4
-  sleep 0.25
-  play :E4
-  sleep 0.25
+sleep 4
+3.times do
+  play :G5
+  sleep 0.6
 end
 
+2.times do
+  play :Ef5
+  sleep 0.45
+  play :Bf5
+  sleep 0.15
+  play :G5
+  sleep 0.6
+end
+live_loop :samp do
+  sample"C:/Users/andres_villa/Desktop/gun_blaster.wav"
+  sleep 5
+end
+sleep 0.5
+
+sleep 0.7
+3.times do
+  play :D6
+  sleep 0.6
+end
+play :Ef6
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+
+play :G6
+sleep 0.6
+play :G5
+sleep 0.45
+play :G5
+sleep 0.15
+play :G6
+sleep 0.6
+play :Gf6
+sleep 0.45
+play :F6
+sleep 0.15
+
+play :E6
+sleep 0.15
+play :Ds6
+sleep 0.15
+play :E6
+
+sleep 0.30
+sleep 0.30
+play :Gs5
+sleep 0.30
+play :Cs6
+sleep 0.6
+play :Bs5
+sleep 0.45
+play :B5
+sleep 0.15
+
+play :Bf5
+sleep 0.15
+play :A5
+sleep 0.15
+play :Bf5
+sleep 0.30
+sleep 0.30
+play :Ef5
+sleep 0.30
+play :Gf5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+play :Ef5
+sleep 0.45
+play :Bf5
+sleep 0.1
+play :G5
+sleep 0.6
